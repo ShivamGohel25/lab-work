37e1f76321e1e5f0cd1cf90dfa5bdacb32b8c8d7
ShivamGohel25 <shivamrgohel25@gmail.com>
2026-01-02 11:53:51 +0530

    Add files via upload

---
+++ 25Cp002/2.c
@@ -0,0 +1,13 @@
+/*wap to sum of two variables*/
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("put value of a: ");
+    scanf("%d",&a);
+    printf("put value of b: ");
+    scanf("%d",&b);
+    int sum;
+    sum = a+b;
+   printf("the sum is %d", sum);
+}
---
+++ 25Cp002/4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+
+    a = 25;
+    b = 4;
+    printf("the sum of the a and b is:%d\n", a+b);
+    printf("the substraction of the a and b is:%d\n", a-b);
+    printf("the multiplication of the a and b is:%d\n", a*b);
+    printf("the division of the a and b is:%.2f\n", a/4.0);
+    printf("the modulo of the a and b is:%d\n", a%b);
+    return 0;
+}
